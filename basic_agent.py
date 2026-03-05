4ee7cda502b438cc2ee1ffb2a0478c905715e6dc
MR473 <mohnish.ramachandran@gmail.com>
2026-03-05 11:32:52 -0500

    Added basic livekit voice agent

---
+++ basic_agent.py
@@ -0,0 +1,40 @@
+
+import os
+from dotenv import load_dotenv
+from livekit import agents
+from livekit.agents import Agent, AgentSession, RunContext
+from livekit.agents.llm import function_tool
+from livekit.plugins import openai, deepgram, silero
+
+load_dotenv(".env")
+
+class Assistant(Agent):
+    """Basic Voice assistant agent for web search"""
+
+    def __init__(self):
+        super().__init__(
+            instructions = """You are a helpful and friendly voice assistnat that responds to the users requests."""
+        )
+
+async def entrypoint(ctx: agents.JobContext):
+    """Entry point for agent"""
+
+    session = AgentSession(
+        stt = deepgram.STT(model="nova-2"),
+        llm = openai.LLM(model=os.getenv("LLM_CHOICE", "gpt-4.1-mini")),
+        tts = openai.TTS(voice="echo"),
+        vad = silero.VAD.load(),    
+    )
+
+    await session.start(
+        room = ctx.room,
+        agent = Assistant()
+    )
+
+    await session.generate_reply(
+        instructions = "Greet the user warmly and ask how ou can help."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
