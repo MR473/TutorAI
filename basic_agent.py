cf3958d14279b9268ca5bb34c27c52194a153717
MR473 <mohnish.ramachandran@gmail.com>
2026-03-06 22:27:26 -0500

    Added Date Time tool function for testing

--- basic_agent.py
+++ basic_agent.py
@@ -9,13 +9,22 @@ from livekit.plugins import openai, deepgram, silero
 load_dotenv(".env")
 
 class Assistant(Agent):
-    """Basic Voice assistant agent for web search"""
+    """Basic Voice assistant agent for tutoring"""
 
     def __init__(self):
         super().__init__(
-            instructions = """You are a helpful and friendly voice assistnat that responds to the users requests."""
+            instructions = """You are a helpful and friendly voice assistant that responds to the users requests and helps them learn WHEN ASKED."""
         )
 
+
+    @function_tool
+    async def get_date_time(self, context: RunContext):
+        """Get the current date and time"""
+
+        from datetime import datetime
+
+        now = datetime.now()
+        return now.strftime("%Y-%m-%d %H:%M:%S")
 async def entrypoint(ctx: agents.JobContext):
     """Entry point for agent"""
 
